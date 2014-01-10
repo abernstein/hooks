b42b033b0228ea3853aeb8cab5101f4b049598fe
Aaron Bernstein <abernstein@godaddy.com>
2014-01-09 18:08:04 -0700

    Updated the script, should be ready to trial run.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Pre-requistes:
 #   git
 #   A set of hooks located in an includes/ folder
-
+#
 # Determine Location of the Shared Hooks Repo
 echo -e "Is this the location of the hooks, `pwd` [ Y | N ]:"
 read toggle
@@ -16,7 +16,6 @@ else
 fi
 hooks=$(ls $PATH_TO_HOOKS/includes)
 hook_types=$(ls $PATH_TO_HOOKS/includes | cut -d'-' -f1)
-echo -e "\nWe have the following hooks available:\n${hook_types[@]}"
 
 # Determine repository to install hooks in
 echo -e "\nSCM project directory [/home/myname/repos/myproject]:"
@@ -30,12 +29,13 @@ if [ -d $PATH_TO_PROJECT ]; then
 
   filetypes=$(find $PATH_TO_PROJECT -type f -name "*.*" | awk -F. '{print $NF}' | sort -u)
   echo -e "\nThe project has these filetypes:\n${filetypes[@]}"
+  echo -e "\nWe have the following hooks available:\n${hook_types[@]}"
 
   # Install Hooks
-  echo -e "\nList desired hooks separated by spaces [ all | php js puppet ]:"
+  echo -e "\nList desired hooks separated by spaces:"
   read -a apply_hooks
 
-  if [ $apply_hooks ]; then
+  if [ "${#apply_hooks[@]}" -gt 0 ]; then
     for hook in ${apply_hooks[@]}; do
       hook_dir="${PATH_TO_PROJECT}/.git/hooks"
       append=`find "${PATH_TO_HOOKS}/includes" -maxdepth 2 -iname "*${hook}*.hook"`
@@ -49,12 +49,14 @@ if [ -d $PATH_TO_PROJECT ]; then
           chmod +x $hook_dir/pre-commit
         fi
       fi;
-      if [ "${install_submodules}" == "Y" ]; then
-        for module in $PATH_TO_PROJECT/*/;
-          do echo "ln -s <path_to_shared>/pre-commit ${module}hooks/pre-commit";
-        done;
-      fi;
     done;
+
+    if [ "${install_submodules}" == "Y" ]; then
+      for module in $PATH_TO_PROJECT/modules/*/; do
+        modulename=`basename $module`
+        ln -s $hook_dir/pre-commit $PATH_TO_PROJECT/.git/modules/${modulename}/hooks/pre-commit;
+      done;
+    fi;
   fi;
 else
   echo "Sorry this project doesn't seem to exist at $PATH_TO_PROJECT"
