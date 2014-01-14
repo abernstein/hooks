c7f025008994fb0d1370e5c8eb6a85215a015cbc
Aaron Bernstein <abernstein@godaddy.com>
2014-01-13 17:06:33 -0700

    Updated to case the Y/N; Removed Readme.md from list of hooks; Confirmed Pre-commit exists prior to appending

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@
 # Determine Location of the Shared Hooks Repo
 echo -e "Is this the location of the hooks, `pwd` [ Y | N ]:"
 read toggle
-if [ "${toggle}" == "Y" ]; then
+
+if [ `echo ${toggle} | tr '[:lower:]' '[:upper:]'` == "Y" ]; then
   PATH_TO_HOOKS=`pwd`
 else
   echo -e "What is the location?"
@@ -29,7 +30,7 @@ if [ -d $PATH_TO_PROJECT ]; then
 
   filetypes=$(find $PATH_TO_PROJECT -type f -name "*.*" | awk -F. '{print $NF}' | sort -u)
   echo -e "\nThe project has these filetypes:\n${filetypes[@]}"
-  echo -e "\nWe have the following hooks available:\n${hook_types[@]}"
+  echo -e "\nWe have the following hooks available:\n${hook_types[@]/*.md/}"
 
   # Install Hooks
   echo -e "\nList desired hooks separated by spaces:"
@@ -40,21 +41,25 @@ if [ -d $PATH_TO_PROJECT ]; then
       hook_dir="${PATH_TO_PROJECT}/.git/hooks"
       append=`find "${PATH_TO_HOOKS}/includes" -maxdepth 2 -iname "*${hook}*.hook"`
 
-      if [ -w $append ]; then
-        dups=`grep "${append}" $hook_dir/pre-commit`
-        if [ "${dups}" == "" ]; then
-          echo -e "\nApplying $hook to pre-commit chain"
-          echo -e "bash $append" >> $hook_dir/pre-commit
-          chmod +x $hook_dir/pre-commit
-        else
-          echo -e "\n$hook is already established"
-        fi;
-      fi;
+      if [ -a $hook_dir/pre-commit ]; then
+        if [ -w $append ]; then
+          dups=`grep "${append}" $hook_dir/pre-commit`
+          if [ "${dups}" == "" ]; then
+            echo -e "\nApplying $hook to pre-commit chain"
+            echo -e "bash $append" >> $hook_dir/pre-commit
+            chmod +x $hook_dir/pre-commit
+          else
+            echo -e "\n$hook is already established"
+          fi
+        fi
+      else
+        echo -e "#!/bin/bash\n" > $hook_dir/pre-commit
+      fi
     done;
 
-    if [ "${install_submodules}" == "Y" ]; then
-      for module in $PATH_TO_PROJECT/*/; do
-        submodule_dir=`egrep "path" $PATH_TO_PROJECT/.gitmodules | head -1 | cut -d'=' -f2`
+    if [ `echo ${install_submodules} | tr '[:lower:]' '[:upper:]'` == "Y" ]; then
+      for module in $PATH_TO_PROJECT/modules/*/; do
+        submodule_dir=`egrep "path" $PATH_TO_PROJECT/.gitmodules | head -1 | cut -d'=' -f2 | tr -d ' '`
         ln -s $hook_dir/pre-commit $PATH_TO_PROJECT/.git/modules/${submodule_dir}/hooks/pre-commit 2>/dev/null;
         if [ $? -gt 0 ]; then
           echo -e "There seems to already be hooks installed here";
